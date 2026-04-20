d579e3dca1aaaf933338a027bee9c2390d711022
Eyjolfur Gudjonsson <eyjolfur.gudjonsson@svenskaspel.se>
2026-04-20 21:06:21 +0200

    download button style breakout svg

--- src/components/download-button.tsx
+++ src/components/download-button.tsx
@@ -1,4 +1,5 @@
 import { getDownloadText } from '../lib/download';
+import DownloadIcon from './download-icon';
 
 const DownloadButton = () => {
   const handleClick = () => {
@@ -13,20 +14,7 @@ const DownloadButton = () => {
       title="Download DuckDuckGo"
       onClick={handleClick}
     >
-      <svg
-        viewBox="0 0 16 16"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        height="16"
-        className="download-icon"
-      >
-        <path
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M8 .5a.75.75 0 01.75.75v8.264l2.986-2.81a.75.75 0 111.028 1.092l-4.25 4a.75.75 0 01-1.028 0l-4.25-4a.75.75 0 111.028-1.092l2.986 2.81V1.25A.75.75 0 018 .5zm-5.25 13a.75.75 0 000 1.5h10.5a.75.75 0 000-1.5H2.75z"
-          fill="currentColor"
-        ></path>
-      </svg>
+      <DownloadIcon />
       Download DuckDuckGo for {getDownloadText() ?? 'your device'}
     </button>
   );
